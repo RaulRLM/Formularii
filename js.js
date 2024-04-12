58f5e46071b42a38da977c69ed74d52e422e488c
RaulRLM <160418382+RaulRLM@users.noreply.github.com>
2024-04-12 12:27:21 +0200

    Update js.js

--- js.js
+++ js.js
@@ -91,10 +91,10 @@ inputFields2
         const fechaIngresada = new Date(fecha);
 
         // Expresión regular para verificar el formato de la fecha (DD/MM/AAAA)
-        const formatoValido = /^([0][1-9]|[12][0-9]|3[01])(\/|-)([0][1-9]|[1][0-2])\2(\d{4})$/;
-        if (!formatoValido) {
-        return "Format DD/MM/AAAA.";
-        }
+         const formatoValido = /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/.test(fecha);
+            if (!formatoValido) {
+                return "Format DD/MM/AAAA.";
+            }
 
         // Verificar si la fecha ingresada es mayor a la fecha actual
         const fechaActual = new Date();
